17dd78a17120e9d06b71239a53750c62ee8e45e0
Eric Goldman <github@youshouldtellmemore.com>
2025-02-10 19:15:08 -0800

    Hide policy to update in a KV store

--- src/index.ts
+++ src/index.ts
@@ -93,19 +93,23 @@ async function update(clientOptions: ClientOptions, newPolicy: IPPolicy): Promis
 	const policyData = await policyResponse.json();
 
 	// Modify the IP rule in the policy
+	console.log('before updated');
 	let updated = false;
 	const newRules = policyData.result.rules.map((rule: any) => {
-		/*if (rule.include && Array.isArray(rule.include)) {
+		console.log('before if rule.include');
+		if (rule.include && Array.isArray(rule.include)) {
+			console.log('before rule.include=rule.include.map');
 			rule.include = rule.include.map((includeRule: any) => {
+				console.log('before includeRule.ip');
 				if (includeRule.ip) {
+					console.log('updating includeRule');
 					includeRule.ip = [newPolicy.content]; // Replace with the new IP
 					updated = true;
 				}
 				return includeRule;
 			});
-		}*/
-		rule.purpose === "ip" ? { ...rule, value: newAllowedIps } : rule
-		//return rule;
+		}
+		return rule;
 	});
 
 	if (!updated) {
